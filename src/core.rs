a82cbeb6ec1997cbec4778c5dd9fead21a442df9
foobles <buzdav2@gmail.com>
2020-03-12 17:55:46 -0500

    made most functions on Dispatcher receive Q where K: Borrow<Q>, for being more generic

--- src/core.rs
+++ src/core.rs
@@ -1,27 +1,52 @@
-pub trait DispatchFunction<P, R>: Fn(&P) -> R + 'static + Sync + Send
-where
-    P: Send + Sync {}
+use std::borrow::Borrow;
+use std::hash::Hash;
 
-impl<T, P, R> DispatchFunction<P, R> for T
-where 
-  T: Fn(&P) -> R + 'static + Sync + Send, 
-  P: Send + Sync {}
-
-pub trait Dispatcher<K, P, R> 
+pub trait DispatchFunction<P: ?Sized, R>: Fn(&P) -> R + 'static + Sync + Send
 where
-    K: std::cmp::Eq
+    P: Send + Sync,
 {
-    fn into_vec(self) -> Vec<(K, Box<dyn DispatchFunction<P, R>> )>;
-    fn remove(&mut self, key: &K);
+}
+
+impl<T, P: ?Sized, R> DispatchFunction<P, R> for T
+where
+    T: Fn(&P) -> R + 'static + Sync + Send,
+    P: Send + Sync,
+{
+}
+
+pub trait Dispatcher<K, P: ?Sized, R>
+where
+    K: Eq + Hash,
+{
+    fn into_vec(self) -> Vec<(K, Box<dyn DispatchFunction<P, R>>)>;
+
+    fn remove<Q: ?Sized>(&mut self, key: &Q)
+    where
+        K: Borrow<Q>,
+        Q: Eq + Hash;
+
     fn insert(&mut self, key: K, item: Box<dyn DispatchFunction<P, R>>);
-    fn contains_key(&self, key: &K) -> bool;
-    fn get(&self, key: &K) -> Option<&Box<dyn DispatchFunction<P, R>>>;
+
+    fn contains_key<Q: ?Sized>(&self, key: &Q) -> bool
+    where
+        K: Borrow<Q>,
+        Q: Eq + Hash;
+
+    fn get<Q: ?Sized>(&self, key: &Q) -> Option<&Box<dyn DispatchFunction<P, R>>>
+    where
+        K: Borrow<Q>,
+        Q: Eq + Hash;
+
     fn len(&self) -> usize;
-    
-    fn call(&self, key: &K, params: &P) -> Option<R> {
+
+    fn call<Q: ?Sized>(&self, key: &Q, params: &P) -> Option<R>
+    where
+        K: Borrow<Q>,
+        Q: Eq + Hash,
+    {
         match self.get(key) {
             Some(func) => Some(func(params)),
-            None => None            
+            None => None,
         }
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,67 +1,79 @@
-//! dispatchtable is a runtime key based collection that contains references to functions. 
+//! dispatchtable is a runtime key based collection that contains references to functions.
 //!
 //! # Quick Start
 //! ```
 //! use dispatchtable::{Dispatch, DispatchTable};
-//! 
+//!
 //! fn add(p: &(u8, u8)) -> u8 { p.0 + p.1 }
 //! fn sub(p: &(u8, u8)) -> u8 { p.0 - p.1 }
-//! 
+//!
 //! fn main() {
-//!   let dispatchtable = DispatchTable::new();
-//! 
-//!   dispatchtable.insert("add", Box::new(add));
-//!   dispatchtable.insert("sub", Box::new(sub));
-//! 
+//!   let mut dispatchtable = DispatchTable::new();
+//!
+//!   dispatchtable.insert("add".to_string(), Box::new(add));
+//!   dispatchtable.insert("sub".to_string(), Box::new(sub));
+//!
 //!   assert_eq!(dispatchtable.call("sub", &(10, 5)), Some(5));
 //! }
 //! ```
 
-
-
+mod core;
 #[cfg(test)]
 mod test;
-mod core;
 
-use std::collections::HashMap;
 pub use crate::core::{DispatchFunction, Dispatcher as Dispatch};
+use std::borrow::Borrow;
+use std::collections::HashMap;
+use std::hash::Hash;
 
-pub struct DispatchTable<K, P, R> {
-    inner: HashMap<K, Box<dyn DispatchFunction<P, R>>>
+pub struct DispatchTable<K, P: ?Sized, R> {
+    inner: HashMap<K, Box<dyn DispatchFunction<P, R>>>,
 }
 
-impl<K, P, R> DispatchTable<K, P, R> {
+impl<K: Hash + Eq, P: ?Sized, R> DispatchTable<K, P, R> {
     pub fn new() -> Self {
         Self {
-            inner: HashMap::new()
+            inner: HashMap::new(),
         }
     }
 }
 
-impl<K, P, R> Dispatch<K, P, R> for DispatchTable<K, P, R>
+impl<K, P: ?Sized, R> Dispatch<K, P, R> for DispatchTable<K, P, R>
 where
-    K: std::hash::Hash + std::cmp::Eq
-{   
+    K: std::hash::Hash + std::cmp::Eq,
+{
     fn len(&self) -> usize {
         self.inner.len()
     }
 
-    fn contains_key(&self, key: &K) -> bool {
+    fn contains_key<Q: ?Sized>(&self, key: &Q) -> bool
+    where
+        K: Borrow<Q>,
+        Q: Eq + Hash,
+    {
         self.inner.contains_key(key)
     }
 
-    fn remove(&mut self, key: &K) {
+    fn remove<Q: ?Sized>(&mut self, key: &Q)
+    where
+        K: Borrow<Q>,
+        Q: Eq + Hash,
+    {
         self.inner.remove(key);
     }
     fn insert(&mut self, key: K, item: Box<dyn DispatchFunction<P, R>>) {
         self.inner.insert(key, item);
     }
 
-    fn get(&self, key: &K) -> Option<&Box<dyn DispatchFunction<P, R>>> {
+    fn get<Q: ?Sized>(&self, key: &Q) -> Option<&Box<dyn DispatchFunction<P, R>>>
+    where
+        K: Borrow<Q>,
+        Q: Eq + Hash,
+    {
         self.inner.get(key)
     }
 
-    fn into_vec(self) -> Vec<(K, Box<dyn DispatchFunction<P, R>> )> {
+    fn into_vec(self) -> Vec<(K, Box<dyn DispatchFunction<P, R>>)> {
         self.inner.into_iter().collect()
     }
 }
